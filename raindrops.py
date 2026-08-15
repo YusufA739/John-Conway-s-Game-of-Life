14850fe6a608ff48c5c1e49f0e89afdab6e0b0a9
Yusuf Ahmed <yusuf65820@gmail.com>
2026-08-15 19:28:51 +0100

    Finished

--- raindrops.py
+++ raindrops.py
@@ -1,101 +1,141 @@
-import random,sys,time,os
+import random,sys,time,os,copy
 from colorama import Fore, Back, Style
-nextline = ""
-currentframe = []
-currentframelengthofraindrop = []
-lengthofraindrop = []
-
-#manually get screen width from user. They copy and paste one line; we use count to detect the cells per row
-for carrier in range(500):
-    sys.stdout.write("0")
 
 
 #change these to change frame size
-#linesperframe = 55#55x209 for fullscreen command prompt for my pc
-#cellsperline = 222
-linesperframe = int(input("type in your height:"))
-cellsperline = len(input("use this to calibrate your width and paste the line length:"))
+linesperframe = 30#55x209 for fullscreen command prompt for my pc
+cellsperline = 30
+# linesperframe = int(input("type in your height:"))
+# cellsperline = len(input("use this to calibrate your width and paste the line length:"))
 
-#change these to alter the length of raindrops and the length in between new raindrops in the same column
-lowestvalue = -1000#increasing the spacing between raindrops in the same column
-stopvalue = 0#value to signify the "stop" length of a raindrop, within lowest value and highest value range
-highestvalue = 10#max len of raindrops
+grid = []
+livelinessGrid = []
+new_livelinessGrid = []
+gridlines = []#for building grid only (see below in for loops)
 
-#change these to change what numbers appear in the raindrops
-lowestraindropnumber = 5
-highestraindropnumber = 5
+for carrier in range(cellsperline):
+    gridlines.append(0)
 
-#change to alter line rest time and frame rest time
+for carrier in range(linesperframe):
+    grid.append(copy.deepcopy(gridlines))
+    livelinessGrid.append(copy.deepcopy(gridlines))
+
+livelinessGrid[6][8] = 1
+livelinessGrid[7][7] = 1
+
+livelinessGrid[7][8] = 1
+
+livelinessGrid[7][9] = 1
+livelinessGrid[8][7] = 1
+# livelinessGrid[8][8] = 1
+livelinessGrid[8][9] = 1
+livelinessGrid[9][8] = 1
+# livelinessGrid[9][8] = 1
+# livelinessGrid[9][9] = 1
+
+#change to alter line rest time and frame rest time (in seconds)
 lineresttime = 0
-frameresttime = 0
+frameresttime = 1
 
 targetFramerate = 40
 currentFramerate = 0
-
-#make the first line
-for placeholder in range(cellsperline):#cells per line is given by user input
-    lengthofraindrop.append(random.randint(lowestvalue,highestvalue))
-    nextline += str(random.randint(lowestraindropnumber,highestraindropnumber))#we need this to know initial raindrop lengths and placements
-
-#make the first frame using the first line
-for i in range(linesperframe):
-    currentframe.append(nextline)#every line starts out the same singular line copied out
-    currentframelengthofraindrop.append(lengthofraindrop.copy())#only first line is necessary, but I will leave for now
 
 frameCount = 0
 file = open("framerate.txt", "w")
 start = time.perf_counter()#for inital measurement, will be wrong until it updates in if statement (reduces frame delays due to extra timer processing)
 
-while True:
+deactivateTop = False # no negative i so no i-1
+deactivateBottom = False# no checking i+1
+deactivateLeft = False# no checking j-1
+deactivateRight = False#no checking j+1
 
-    # calculate the next frame's data (changes impacting frame: new line added at top, deletion at bottom)
-    nextline = ""
-    lengthofraindrop = currentframelengthofraindrop[0].copy()
-    for i in range(cellsperline):
-        if lengthofraindrop[i] > stopvalue:
-            if highestraindropnumber > lowestraindropnumber:
-                nextline += str(random.randint(lowestraindropnumber, highestraindropnumber))  # change to (0,1) for binary
-                lengthofraindrop[i] -= 1
-            else:
-                nextline += str(lowestraindropnumber)
-                lengthofraindrop[i] -= 1
-        elif lengthofraindrop[i] <= lowestvalue:
-            if highestraindropnumber > lowestraindropnumber:
-                nextline += str(random.randint(lowestraindropnumber, highestraindropnumber))  # change to (0,1) for binary
-                lengthofraindrop[i] = random.randint(stopvalue, highestvalue)
-            else:
-                nextline += str(lowestraindropnumber)
-                lengthofraindrop[i] = random.randint(stopvalue, highestvalue)
-        else:
-            nextline += str(lowestraindropnumber)
-            lengthofraindrop[i] -= 1
-
-    # update future frame with new information
-    for carrier in range(linesperframe - 1, 0, -1):  # update all unaltered lines down, skipping overwriting the first, so line 1 and 2 will be identical for now, and also not rewriting the last line to another line
-        currentframe[carrier] = currentframe[carrier - 1]
-        currentframelengthofraindrop[carrier] = currentframelengthofraindrop[carrier - 1].copy()
-
-    # finally, update the next frame's first line
-    currentframe[0] = nextline
-    currentframelengthofraindrop[0] = lengthofraindrop.copy()  # tracks droplength remaining to generate. Tells us when the raindrop has reached zero length. Once zero length, we wait until it goes past a certain negative
-    # negative acts like the reverse of a drop. So just black bg/a gap. Once we hit the negative floor, we choose a random positive int to make a new droplet
-
-
-    #display next frame data (updated frame data) (next is current frame)
-    color = 1
+def drawframe(new_livelinessGrid,lineresttime,frameresttime):
+    global linesperframe, cellsperline
+    # display next frame data
     for line in range(linesperframe):
         for cell in range(cellsperline):
-            if currentframelengthofraindrop[line][cell] <= 0:
-                sys.stdout.write(Fore.BLACK + currentframe[line][cell])
+            if new_livelinessGrid[line][cell] == 0:
+                sys.stdout.write(Fore.BLACK + str(new_livelinessGrid[line][cell]))
             else:
-                #color = random.randint(0,1)
-                if color == 0:
-                    sys.stdout.write(Fore.BLACK + currentframe[line][cell])
-                elif color == 1:
-                    sys.stdout.write(Fore.GREEN + currentframe[line][cell])
+                sys.stdout.write(Fore.WHITE + str(new_livelinessGrid[line][cell]))
         sys.stdout.write("\n")
         time.sleep(lineresttime)
     time.sleep(frameresttime)
+
+#unused so far (from old raindrops code)
+#change these to change what numbers appear in the raindrops
+lowestraindropnumber = 0
+highestraindropnumber = 9
+if highestraindropnumber > lowestraindropnumber:
+    highestraindropnumber = lowestraindropnumber
+
+
+while True:
+    new_livelinessGrid = copy.deepcopy(livelinessGrid)
+
+
+    for i in range(linesperframe):
+        for j in range(cellsperline):
+            deactivateTop = False  # no negative i so no i-1
+            deactivateBottom = False  # no checking i+1
+            deactivateLeft = False  # no checking j-1
+            deactivateRight = False  # no checking j+1
+            NeighbourCount = 0
+            # if i == 0:
+            #     if j == 0:
+            #         NeighbourCount += livelinessGrid[i + 1][j]
+            #         NeighbourCount += livelinessGrid[i + 1][j + 1]
+            #         NeighbourCount += livelinessGrid[i + 1][j + 1]
+            #     elif j > 0:
+            #         NeighbourCount += livelinessGrid[i][j - 1]
+            #         NeighbourCount += livelinessGrid[i + 1][j - 1]
+            #         NeighbourCount += livelinessGrid[i + 1][j]
+            #         NeighbourCount += livelinessGrid[i + 1][j + 1]
+            #         NeighbourCount += livelinessGrid[i][j + 1]
+            #didnt bother completing as ive simplified the redundancy between neighbour checking below
+
+            if i == 0:
+                deactivateTop = True
+            if i == linesperframe - 1:
+                deactivateBottom = True
+            if j == 0:
+                deactivateLeft = True
+            if j == cellsperline - 1:
+                deactivateRight = True
+
+            if deactivateLeft == False:
+                NeighbourCount += livelinessGrid[i][j - 1]
+            if deactivateBottom == False and deactivateLeft == False:
+                NeighbourCount += livelinessGrid[i + 1][j - 1]
+            if deactivateBottom == False:
+                NeighbourCount += livelinessGrid[i + 1][j]
+            if deactivateBottom == False and deactivateRight == False:
+                NeighbourCount += livelinessGrid[i + 1][j + 1]
+            if deactivateRight == False:
+                NeighbourCount += livelinessGrid[i][j + 1]
+            if deactivateTop == False and deactivateRight == False:
+                NeighbourCount += livelinessGrid[i - 1][j + 1]
+            if deactivateTop == False:
+                NeighbourCount += livelinessGrid[i - 1][j]
+            if deactivateTop == False and deactivateLeft == False:
+                NeighbourCount += livelinessGrid[i - 1][j - 1]
+
+            #above can be further simplified, as can below. as above, so below
+            #(idk i heard this in uncharted 3 but apparently they took it from a latin quote about
+            #the macro scale universal attributes and micro scale universal attributes)
+
+            if NeighbourCount < 2:
+                new_livelinessGrid[i][j] = 0
+            elif (NeighbourCount == 2 or NeighbourCount == 3) and livelinessGrid[i][j] == 1:#can stay alive as it has 2 or 3 neighbours
+                new_livelinessGrid[i][j] = 1
+            elif NeighbourCount == 3 and livelinessGrid[i][j] == 0:#needs exactly 3 neighbours to be born
+                new_livelinessGrid[i][j] = 1
+            elif NeighbourCount > 3:# >= 4 for accuracy to Conway's description of the rules on numberphile
+                new_livelinessGrid[i][j] = 0
+
+
+    #draw frame using data
+    drawframe(new_livelinessGrid,lineresttime,frameresttime)
 
 
     os.system("cls")#this needs to be last operation so that more time is spent as displaying vs more time spent showing blank screen
@@ -103,6 +143,8 @@ while True:
 
     #end = time.perf_counter()#if only 1 frame
     #all of nextline and future frame calculations have been done. Track framerate now (overhead from file.write could be added, but it won't be for now)
+
+    livelinessGrid = copy.deepcopy(new_livelinessGrid)
 
     frameCount += 1
 
