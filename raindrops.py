59afbb78e10df4ced78ac6dff0ce56319054557d
Yusuf Ahmed <yusuf65820@gmail.com>
2026-08-16 16:54:07 +0100

    changes to drawFrame and frameCount

--- raindrops.py
+++ raindrops.py
@@ -3,22 +3,28 @@ from colorama import Fore, Back, Style
 
 
 #change these to change frame size
-linesperframe = 30#55x209 for fullscreen command prompt for my pc
-cellsperline = 30
+linesperframe = 50#55x209 for fullscreen command prompt for my pc
+cellsperline = 200
 # linesperframe = int(input("type in your height:"))
 # cellsperline = len(input("use this to calibrate your width and paste the line length:"))
 
-grid = []
+grid = []#used to keep all pixels zero
 livelinessGrid = []
 new_livelinessGrid = []
 gridlines = []#for building grid only (see below in for loops)
+livelines = []#for building grid only (see below in for loops)
 
 for carrier in range(cellsperline):
     gridlines.append(0)
 
 for carrier in range(linesperframe):
     grid.append(copy.deepcopy(gridlines))
-    livelinessGrid.append(copy.deepcopy(gridlines))
+
+for carrier in range(linesperframe):
+    for carrier in range(cellsperline):
+        livelines.append(random.randint(0,1))
+    livelinessGrid.append(copy.deepcopy(livelines))
+    livelines = []
 
 livelinessGrid[6][8] = 1
 livelinessGrid[7][7] = 1
@@ -34,8 +40,9 @@ livelinessGrid[9][8] = 1
 # livelinessGrid[9][9] = 1
 
 #change to alter line rest time and frame rest time (in seconds)
+cellresttime = 0
 lineresttime = 0
-frameresttime = 1
+frameresttime = 0
 
 targetFramerate = 40
 currentFramerate = 0
@@ -49,18 +56,37 @@ deactivateBottom = False# no checking i+1
 deactivateLeft = False# no checking j-1
 deactivateRight = False#no checking j+1
 
-def drawframe(new_livelinessGrid,lineresttime,frameresttime):
-    global linesperframe, cellsperline
+#wont automatically clear frame as we allow user to have finer control over when they want it cleared
+#tradeoff between finer control and less controls is more vs less complexity, respectively
+def drawframe(imageData,crtGiven=None,lrtGiven=None,frtGiven=None,lpfGiven=None,cplGiven=None):
+    global cellresttime, lineresttime, frameresttime, linesperframe, cellsperline
+    if crtGiven is None:
+        crtGiven = cellresttime
+    if lrtGiven is None:
+        lrtGiven = lineresttime
+    if frtGiven is None:
+        frtGiven = frameresttime
+    if lpfGiven is None:
+        lpfGiven = linesperframe
+    if cplGiven is None:
+        cplGiven = cellsperline
     # display next frame data
-    for line in range(linesperframe):
-        for cell in range(cellsperline):
-            if new_livelinessGrid[line][cell] == 0:
-                sys.stdout.write(Fore.BLACK + str(new_livelinessGrid[line][cell]))
+    for line in range(lpfGiven):
+        for cell in range(cplGiven):
+            if imageData[line][cell] == 0:
+                # sys.stdout.write(Fore.BLACK + str(imageData[line][cell]))
+                sys.stdout.write(Fore.BLACK + "0")
             else:
-                sys.stdout.write(Fore.WHITE + str(new_livelinessGrid[line][cell]))
+                # sys.stdout.write(Fore.WHITE + str(imageData[line][cell]))
+                sys.stdout.write(Fore.WHITE + "0")
+            time.sleep(crtGiven)#sleep for cell
         sys.stdout.write("\n")
-        time.sleep(lineresttime)
-    time.sleep(frameresttime)
+
+        time.sleep(lrtGiven)#sleep for line
+    time.sleep(frtGiven)#sleep for frame
+
+def clearframe():
+    os.system("cls")
 
 #unused so far (from old raindrops code)
 #change these to change what numbers appear in the raindrops
@@ -135,10 +161,10 @@ while True:
 
 
     #draw frame using data
-    drawframe(new_livelinessGrid,lineresttime,frameresttime)
+    drawframe(new_livelinessGrid,cellresttime,lineresttime,frameresttime,linesperframe,cellsperline)#linesperframe is basically how many y then cellsperline is how many x
 
 
-    os.system("cls")#this needs to be last operation so that more time is spent as displaying vs more time spent showing blank screen
+    clearframe()#this needs to be last operation so that more time is spent as displaying vs more time spent showing blank screen
     #frame is over and wiped the screen for the next frame's preparation
 
     #end = time.perf_counter()#if only 1 frame
@@ -151,7 +177,7 @@ while True:
     if frameCount % targetFramerate == 0:
         end = time.perf_counter()
         delta = end - start
-        correctedDelta = delta / targetFramerate
+        correctedDelta = delta / frameCount
         file.write("Target: "+str(targetFramerate)+"\n"+"Actual: "+str(1/correctedDelta)+"\n")
         file.flush()#will force the buffer to write to file so that if the program is closed without closing file, it will still save the last result
         start = time.perf_counter()
